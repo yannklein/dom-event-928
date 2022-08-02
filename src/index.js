a234a0bd572892434df1790ab49775133eed5c7b
Yann Klein <yann.klein@me.com>
2022-08-02 10:45:13 +0900

    lecture done

--- src/index.js
+++ src/index.js
@@ -1,54 +1,52 @@
-console.log("Hi Batch #858!!!!");
+// console.log("Hi Batch #928!!!!");
 
 // ////////
 // Select DOM elements
 // ////////
 
-// with its ID (Shinji)
-const shinji = document.querySelector("#shinji");
-console.log(shinji);
+// with its ID (Yaya)
+// const yaya = document.querySelector("#yaya");
+// console.log(yaya);
 
-// with its tag type (h2)
-const h2 = document.querySelector("h2");
-console.log(h2);
+// // with its tag type (h2)
+// const h2 = document.querySelector("h2");
+// console.log(h2);
 
-// with its class (group)
-const group = document.querySelector(".group");
-console.log(group);
+// // with its class (group)
+// const group = document.querySelector(".group");
+// console.log(group);
 
-// by nest selection (ul of the group)
-const ul = group.querySelector("ul");
-console.log(ul);
+// // by nest selection (ul of the group)
+// const ul = group.querySelector("ul");
+// console.log(ul);
 
-// select multiple elements (all the card)
-const cardList = document.querySelectorAll(".card");
-console.log(cardList);
+// // select multiple elements (all the card)
+// const cards = document.querySelectorAll(".card");
+// console.log(cards);
+
 
 // ////////
 // Modify the DOM
 // ////////
 
-// add an HTML element (Juventus de MediRechi)
-const list = document.querySelector(".group ul");
-console.log(list);
-list.insertAdjacentHTML(
-  "beforeend",
-  "<li><em>Juventus</em> de MediRechi 🇮🇹</li>"
-);
+// add an HTML element (Jobs United ⚙️)
+const teamListUl = document.querySelector(".group ul");
+teamListUl.insertAdjacentHTML("beforeend", "<li>Jobs <em>United</em> ⚙️</li>");
 
-// same but more complex (add James!!!)
-const nurtureCard = document.querySelector("#nurture ul");
-nurtureCard.insertAdjacentHTML(
-  "beforeend",
+// same but more complex (add Josh!!!)
+const susthamUl = document.querySelector("#sustham ul");
+susthamUl.insertAdjacentHTML(
+  "afterbegin",
   `<li>
-    <img src="https://avatars.githubusercontent.com/u/74216026?v=4" alt="">
-    <p>James</p>
+    <img src="https://avatars.githubusercontent.com/u/104883545?v=4" alt="">
+    <p>Josh</p>
   </li>`
 );
 
 // change the element style (group background to green)
 // Careful! background-color => backgroundColor
-group.style.backgroundColor = "green";
+const group = document.querySelector(".group");
+group.style.backgroundColor = "darkgreen";
 
 // add a class to the element (add text-white to group)
 // Careful! no DOT for classList
@@ -60,52 +58,48 @@ group.classList.remove("text-white");
 // add/remove every 1sec
 // setInterval(() => {
 //   group.classList.toggle("text-white");
-// }, 1000);
+// }, 3000);
 
 // Read/Write for inputs (change the value in the email input)
-const emailInput = document.querySelector("#email");
-console.log(emailInput.value);
-emailInput.value = "yann@lewagon.org";
+const mail = document.querySelector("#email");
+console.log(mail.value);
 
-// Read/Write the (inner) text and HTML content (FF Ronin 🥷 title)
-const roninTitle = document.querySelector("#ff-ronin h3");
-// console.log(roninTitle);
-// console.log(roninTitle.innerText);
-// console.log(roninTitle.innerHTML);
-// console.log(group.innerHTML);
-// group.innerHTML = ""; // to remove everythjing in the div group
-// group.remove() // remove the  whole div group
-roninTitle.innerHTML = "Hello <strong>everyone</strong>";
+mail.value = "yann@lewagon.org";
+
+// Read/Write the (inner) text and HTML content (FF Healthish title)
+const healthish = document.querySelector("#ff-healthish h3");
+console.log(healthish.value); // NOT WORKING except for inputs!
+console.log(healthish.innerHTML);
+console.log(healthish.innerText);
+
+// healthish.innerText = "Hi <strong>everyone</strong>"; // SHOWS HTML CODE AS A TEXT
+healthish.innerHTML = "Hi <strong>everyone</strong>";
 
 // ////////
 // Add event listeners
 // ////////
 
-// // 1. Select elements (Shinji's picture)
-// const shinjiPic = document.querySelector("#shinji");
+// 1. Select an element (yaya picture)
+// const yaya = document.querySelector("#yaya");
 
-// // 2. Listen to an event (a click on Shinji's picture)
-// shinjiPic.addEventListener("click", (event) => {
-//   // this is a callback function
-//   // it is called only when the user click
+// // 2. Listen to an event (click on yaya picture)
+// yaya.addEventListener("click", (event) => {
 //   console.log(event);
-//   console.log(event.currentTarget); // the element we clicked
-//   // 3. Change the DOM (add the .selected class top Shinji's picture)
-//   event.currentTarget.classList.toggle("selected");
+//   // 3. Change the DOM (add a .selected class to yaya pic)
+//   // event.currentTarget represents the clicked element, in our case yaya's picture
+//   const clickedElement = event.currentTarget;
+//   clickedElement.classList.toggle("selected");
 // });
 
-// 1. Select elements (Shinji's picture)
-const pictures = document.querySelectorAll(".card img");
+// 1. Select elements (all the pictures)
 
+const pictures = document.querySelectorAll(".card img");
 pictures.forEach((picture) => {
-  // 2. Listen to an event (a click on Shinji's picture)
   picture.addEventListener("click", (event) => {
-    // this is a callback function
-    // it is called only when the user click
     console.log(event);
-    // debugger
-    console.log(event.currentTarget); // the element we clicked
-    // 3. Change the DOM (add the .selected class top Shinji's picture)
-    event.currentTarget.classList.toggle("selected");
+    // 3. Change the DOM (add a .selected class to yaya pic)
+    // event.currentTarget represents the clicked element, in our case yaya's picture
+    const clickedElement = event.currentTarget;
+    clickedElement.classList.toggle("selected");
   });
 });
